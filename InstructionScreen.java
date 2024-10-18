1f862c81dfe12c15dafbd00727609b8c2f8038e7
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-18 13:02:05 -0400

    adding titles

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,8 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class InstructionScreen extends World
 {
+    private MenuScreen menu;
+    private String[] instructions;
+    private int index;
     public InstructionScreen(MenuScreen menu)
     {
         super(600, 400, 1);
+        this.menu = menu;
+        instructions = new String[]
+        {
+            "Welcome to the game!",
+            "Use arrow keys to move.",
+            "Press space to jump."
+        };
+        index = 0;
     }
 }
