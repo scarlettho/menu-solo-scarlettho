70c349b318a44ca0a8e7935e35db2dca60713a71
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-19 18:49:26 -0400

    Update MenuScreen.java

--- MenuScreen.java
+++ MenuScreen.java
@@ -7,12 +7,22 @@ public class MenuScreen extends World {
     public MenuScreen() {
         super(600, 400, 1);
         addObject(new Button(this::goInstructions), 300, 340);
+        
         avatars = new LinkedList<>();
         avatars.add(new GreenfootImage("face_a.png"));
         avatars.add(new GreenfootImage("face_b.png"));
         avatars.add(new GreenfootImage("face_c.png"));
+        
+        currentAvatar = avatars.peek();
+        setBackground(currentAvatar);
+        addObject(new Button(this::cycleAvatar, "Next Avatar"), 300, 300);
     }
     public void goInstructions() {
         Greenfoot.setWorld(new InstructionScreen(this));
+    }
+    public void cycleAvatar() {
+        avatars.add(avatars.remove()); // Rotate the avatars in the queue
+        currentAvatar = avatars.peek();
+        setBackground(currentAvatar);  // Update the background with the new avatar
     }
 }
