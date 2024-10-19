132da701b373362be7a614717c418966f3a4a219
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-19 18:46:16 -0400

    adding avatars

--- MenuScreen.java
+++ MenuScreen.java
@@ -2,11 +2,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.Queue;
 import java.util.LinkedList;
 public class MenuScreen extends World {
-  public MenuScreen() {
-    super(600, 400, 1);
-    addObject(new Button(this::goInstructions), 300, 340);
-  }
-  public void goInstructions() {
-    Greenfoot.setWorld(new InstructionScreen(this));
-  }
+    private Queue<GreenfootImage> avatars;
+    private GreenfootImage currentAvatar;
+    public MenuScreen() {
+        super(600, 400, 1);
+        addObject(new Button(this::goInstructions), 300, 340);
+        avatars = new LinkedList<>();
+        avatars.add(new GreenfootImage("face_a.png"));
+        avatars.add(new GreenfootImage("face_b.png"));
+        avatars.add(new GreenfootImage("face_c.png"));
+    }
+    public void goInstructions() {
+        Greenfoot.setWorld(new InstructionScreen(this));
+    }
 }
