bbff9db0b310dd8fef93e72894f6e868d3875179
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-19 14:44:14 -0400

    update instructions

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -4,6 +4,7 @@ public class InstructionScreen extends World
     private MenuScreen menu;
     private String[] instructions;
     private int index;
+    private Label labelInstruction;
     public InstructionScreen(MenuScreen menu)
     {
         super(600, 400, 1);
@@ -15,5 +16,24 @@ public class InstructionScreen extends World
             "Press space to jump."
         };
         index = 0;
+        
+        labelInstruction = new Label(instructions[index], 30);
+        addObject(labelInstruction, 300, 200);
+    }
+    public void nextInstruction() 
+    {
+        if (index < instructions.length - 1)
+        {
+            index++;
+            labelInstruction.setValue(instructions[index]);
+        }
+    }
+    public void previousInstruction()
+    {
+        if (index > 0)
+        {
+            index--;
+            labelInstruction.setValue(instructions[index]);
+        }
     }
 }
