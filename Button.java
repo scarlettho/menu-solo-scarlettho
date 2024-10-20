1c1b23e5c258ccecb5a0459cb2eaea29b1d592dc
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-20 14:34:24 -0400

    Update Button.java

--- Button.java
+++ Button.java
@@ -1,8 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Button extends Actor {
     private Runnable action;
-
-    public Button(Runnable action) {
+    public Button(Runnable action, String label) {
         this.action = action;
         GreenfootImage image = new GreenfootImage("images/buttonLong_beige.png");
         setImage(image);
