37128d7135c3c1e5a9d7d5491001b2f181224559
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-20 14:52:27 -0400

    Update InstructionScreen.java

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,26 +1,44 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class InstructionScreen extends World
 {
-    private MenuScreen menu;
     private String[] instructions;
     private int index;
-    private Label labelInstruction;
+    private Label label;
     public InstructionScreen(MenuScreen menu)
     {
         super(600, 400, 1);
-        this.menu = menu;
         instructions = new String[]{"Welcome to the game!","Use arrow keys to move.", "Press space to jump."}; 
         index = 0;
         
-        labelInstruction = new Label(instructions[index], 30);
-        addObject(labelInstruction, 300, 200);
+        label = new Label(instructions[index], 30);
+        addObject(label, 300, 200);
+        buttons();
+    }
+    public void buttons()
+    {
+        Button nextButton = new Button("Next", this::nextInstruction);
+        addObject(nextButton, getWidth()/2, + 100, getHeight() - 50);
+        
+        Button prevButton = new Button("Previous", this::previousInstruction);
+        addObject(prevButton, getWidth()/2, - 100, getHeight() - 50);
+    }
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(getObjects(Button.class).get(0)))
+        {
+            nextInstruction();
+        }
+        if(Greenfoot.mouseClicked(getObjects(Button.class).get(0)))
+        {
+            previousInstruction();
+        }
     }
     public void nextInstruction() 
     {
         if (index < instructions.length - 1)
         {
             index++;
-            labelInstruction.setValue(instructions[index]);
+            label.setValue(instructions[index]);
         }
     }
     public void previousInstruction()
@@ -28,7 +46,7 @@ public class InstructionScreen extends World
         if (index > 0)
         {
             index--;
-            labelInstruction.setValue(instructions[index]);
+            label.setValue(instructions[index]);
         }
     }
 }
