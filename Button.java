df0bc59eded3a16bf9906164ff62607d4115e9ae
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-21 19:21:56 -0400

    Update Button.java

--- Button.java
+++ Button.java
@@ -2,25 +2,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Button extends Actor {
     private Runnable action;
-
+    private String label;
     // Constructor: the label comes first, then the action
     public Button(String label, Runnable action) {
         this.action = action;
+        this.label = label;
         
         // Set the button image
         GreenfootImage image = new GreenfootImage("images/buttonLong_beige.png");
         image.setColor(Color.BLACK);
-        image.setFont(new Font(20));
+        image.setFont(new Font("Arial", true, false, 20));
         
         // Draw the label on the button
-        image.drawString(label, image.getWidth() / 3, image.getHeight() / 2 + 5);
+        image.drawString(label, image.getWidth() / 2 - 50, image.getHeight() / 2);
         setImage(image); 
     }
 
     // Detect mouse click and perform action
     public void act() {
         if (Greenfoot.mouseClicked(this)) {
-            action.run();  // Trigger the action passed in the constructor
+            if(action != null)
+            {
+                action.run();  // Trigger the action passed in the constructor
+            }
         }
     }
 }
