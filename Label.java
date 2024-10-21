83a10efa8915bbcc1fb07112e394ab506bfacdf2
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-21 18:56:54 -0400

    Update Label.java

--- Label.java
+++ Label.java
@@ -12,28 +12,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Label extends Actor
 {
     private GreenfootImage image;
-    private String value;
+    private String text;
     private int fontSize;
-    private Color lineColor = Color.BLACK;
-    private Color fillColor = Color.WHITE;
-    
-    private static final Color transparent = new Color(0,0,0,0);
-
-    
-    /**
-     * Create a new label, initialise it with the int value to be shown and the font size 
-     */
-    public Label(int value, int fontSize)
-    {
-        this(Integer.toString(value), fontSize);
-    }
     
     /**
      * Create a new label, initialise it with the needed text and the font size 
      */
     public Label(String value, int fontSize)
     {
-        this.value = value;
+        this.text = text;
         this.fontSize = fontSize;
         updateImage();
     }
@@ -43,52 +30,18 @@ public class Label extends Actor
      * 
      * @param value the text to be show
      */
-    public void setValue(String value)
+    public void setValue(String text)
     {
-        this.value = value;
+        this.text = text;
         updateImage();
     }
     
-    /**
-     * Sets the value as integer
-     * 
-     * @param value the value to be show
-     */
-    public void setValue(int value)
-    {
-        this.value = Integer.toString(value);
-        updateImage();
-    }
-    
-    /**
-     * Sets the line color of the text
-     * 
-     * @param lineColor the line color of the text
-     */
-    public void setLineColor(Color lineColor)
-    {
-        this.lineColor = lineColor;
-        updateImage();
-    }
-    
-    /**
-     * Sets the fill color of the text
-     * 
-     * @param fillColor the fill color of the text
-     */
-    public void setFillColor(Color fillColor)
-    {
-        this.fillColor = fillColor;
-        updateImage();
-    }
-    
-
     /**
      * Update the image on screen to show the current value.
      */
     private void updateImage()
     {
-        image = new GreenfootImage(value, fontSize, Color.BLACK, Color.WHITE);
+        image = new GreenfootImage(text, fontSize, Color.RED, new Color(0, 0, 0, 0));
         setImage(image);
     }
 }
